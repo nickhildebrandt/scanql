dc8cf801c3d067ba0570a15adf620e084c4cb24e
Nick Hildebrandt <N.Hildebrandt@ts13.de>
2026-01-30 08:44:09 +0100

    Add additional invalid SQL cases for testing and enhance valid SQL examples

--- sql/invalid.sql
+++ sql/invalid.sql
@@ -5,3 +5,21 @@ SELECT password FROM user WHERE user = 'xxxx' OR '1'='1';
 DELETE FROM users WHERE user = 'username';
 
 SELECT password FROM user WHERE user = 'xxxx'; DROP TABLE users; -- ';
+
+-- Additional invalid cases for testing
+SELECT FROM missing_columns;
+SELECT a b c FROM t; -- missing commas
+SELECT * WHERE no_table = 1; -- missing FROM
+SELECT col FROM ; -- missing table name
+SELECT col FROM t WHERE = 5; -- missing lhs
+SELECT col FROM t WHERE col = ; -- missing rhs
+SELECT col FROM t WHERE col = 'unterminated; -- unterminated string
+SELECT col FROM t, WHERE col = 1; -- stray comma before WHERE
+SELECT col FROM t WHERE col == 1; -- double equals not supported
+SELECT col FROM t WHERE col = 1 AND; -- dangling AND
+SELECT col FROM t WHERE col = 1 OR; -- dangling OR
+FROM t SELECT col; -- wrong order
+UPDATE t SET a = 1; -- unsupported statement type
+DROP TABLE t; -- unsupported statement type
+INSERT INTO t VALUES (1,2,3); -- unsupported statement type
+CREATE TABLE t (id INT); -- unsupported statement type
--- sql/valid.sql
+++ sql/valid.sql
@@ -33,6 +33,23 @@ CREATE TABLE shop (
 INSERT INTO shop VALUES
     (1,'A',3.45),(1,'B',3.99),(2,'A',10.99),(3,'B',1.45),
     (3,'C',1.69),(3,'D',1.25),(4,'D',19.95);
+
+-- Additional simple SELECTs that fit the minimal validator subset
+SELECT id, name FROM customers WHERE id = 1;
+SELECT * FROM users WHERE age = 30 AND city = 'Berlin';
+SELECT a, b FROM t WHERE a = 1 OR b = 2;
+SELECT col FROM tbl WHERE col = 123;
+
+-- Star and multiple columns
+SELECT *, col1 FROM demo WHERE col2 = 'x';
+SELECT col1, col2, col3 FROM demo_table WHERE col3 = 42;
+
+-- Chained AND/OR
+SELECT x FROM foo WHERE a = 1 AND b = 2 OR c = 3;
+SELECT user FROM accounts WHERE active = 1 AND suspended = 0;
+
+-- Trailing semicolon termination
+SELECT value FROM config WHERE key = 'site_name';
 SELECT user, host FROM user;
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `eastwind` /*!40100 DEFAULT CHARACTER SET latin1 */;
